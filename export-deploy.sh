631a1bf9bc957ca37ea0c0244b385e5ac67ef8e6
matrix-ops <58019420+matrix-ops@users.noreply.github.com>
2021-09-22 14:34:44 +0800

    Update export-deploy.sh

--- export-deploy.sh
+++ export-deploy.sh
@@ -6,6 +6,6 @@ mkdir -p ${exportDir} &> /dev/null
 for i in $(kubectl get deploy -n ${namespace} | awk 'NR>1{print $1}');do
         kubectl get deploy -n ${namespace} $i -o yaml > ${exportDir}/${namespace}-$i.yaml
         cat ${exportDir}/${namespace}-$i.yaml | \
-        yq e 'del(.metadata.managedFields) | del(.status) | del(.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration")'  - | \
+        yq e 'del(.metadata.managedFields) | del(.status) | del(.metadata.annotations."kubectl.kubernetes.io/last-applied-configuration") | del (.metadata.resourceVersion) | del (.metadata.creationTimestamp)'  - | \
         tee ${exportDir}/${namespace}-$i.yaml
 done
